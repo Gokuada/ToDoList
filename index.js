196f1c250f743014eb32a0daacda2fbfc14b600b
Gokuada <124284809+Gokuada@users.noreply.github.com>
2023-06-20 14:59:54 +0200

    integration js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
- 
+const input = document.getElementById("input");
+const container = document.getElementById("list-container")
+console.log(container)
+
+function addTask() {
+
+    if (input.value === '') {
+        alert ('Vous devez écrire quelque chose ')
+    } else {
+        let li= document.createElement('li');
+        li.innerHTML= input.value;
+        container.appendChild(li); 
+    }
+    input.value=""
+    console.log(addTask)  
+}
